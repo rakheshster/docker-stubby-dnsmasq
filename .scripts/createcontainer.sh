de57558e19ca84e2e0356d27ea30f3c61aa0f9e9
Rakhesh Sasidharan <me@rakhesh.com>
2021-02-19 19:18:28 +0000

    fixed createcontainer script

--- .scripts/createcontainer.sh
+++ .scripts/createcontainer.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # create a docker volume for storing data. this is automatically named after the container plus a suffix. 
-UNBOUND_DATA=${NAME}_unbounddata && docker volume create $UNBOUND_DATA
+DNSMASQ_DATA=${NAME}_dnsmasqdata && docker volume create $DNSMASQ_DATA
 STUBBY_DATA=${NAME}_stubbydata && docker volume create $STUBBY_DATA
 
 if [[ -z "$4" ]]; then 
@@ -33,26 +33,27 @@ else
     NETWORK=$4
 fi
 
+IP=$3
 if [[ -z "$3" ]]; then
     docker create --name "$NAME" \
         -P --network="$NETWORK" \
         -e TZ="Europe/London" \
         --restart=unless-stopped \
-        --mount type=volume,source=$UNBOUND_DATA,target=/etc/unbound.d \
+        --cap-add=NET_ADMIN \
+        --mount type=volume,source=$DNSMASQ_DATA,target=/etc/dnsmasq.d \
         --mount type=volume,source=$STUBBY_DATA,target=/etc/stubby \
         "$IMAGE"
 else
-    IP=$3
     docker create --name "$NAME" \
-        --ip="$IP" -P \
-        --network="$NETWORK" \
+        -P --network="$NETWORK" --ip=$IP \
         -e TZ="Europe/London" \
         --restart=unless-stopped \
-        --mount type=volume,source=$UNBOUND_DATA,target=/etc/unbound.d \
+        --cap-add=NET_ADMIN \
+        --mount type=volume,source=$DNSMASQ_DATA,target=/etc/dnsmasq.d \
         --mount type=volume,source=$STUBBY_DATA,target=/etc/stubby \
         "$IMAGE"
 fi
-# Note that the container already has a /etc/unbound.d folder which contains files copied in during the image build.
+# Note that the container already has a /etc/dnsmasq.d folder which contains files copied in during the image build.
 # When I create the docker volume above and map it to the container, if this volume is empty the files from within the container are copied over to it.
 # Subsequently the files from the volume are used in preference to the files in the image. 
 # Ditto for /etc/stubby. 
@@ -64,18 +65,19 @@ printf "\nTo start the container do: \n\tdocker start $NAME"
 
 printf "\n\nCreating ./${NAME}.service for systemd"
 cat <<EOF > $NAME.service
-    [Unit]
-    Description=Stubby Unbound Container
-    Requires=docker.service
-    After=docker.service
+[Unit]
+Description=Stubby Unbound Container
+Requires=docker.service
+After=docker.service
 
-    [Service]
-    Restart=on-abort
-    ExecStart=/usr/bin/docker start -a $NAME
-    ExecStop=/usr/bin/docker stop -t 2 $NAME
+[Service]
+Restart=on-abort
+ExecStart=/usr/bin/docker start -a $NAME
+ExecStop=/usr/bin/docker stop -t 2 $NAME
+ExecReload=/usr/bin/docker exec $NAME s6-svc -h /var/run/s6/services/dnsmasq
 
-    [Install]
-    WantedBy=local.target
+[Install]
+WantedBy=local.target
 EOF
 
 printf "\n\nDo the following to install this in systemd & enable:"
